c0228430dedd7d80beeb468de51051b5cc9c2ace
Diego Vieira <dvsdiegovieira@gmail.com>
2024-04-25 07:36:25 -0300

    update: modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { setCurrentEnvironment } from './infra/config/environments';
 import { Decrypter } from './core/domain/protocols/cryptography/cryptography';
 import { CategoryModule } from './infra/ioc/category/category.module';
+import { AttributesModule } from './infra/ioc/attributes/attributes.module';
+import { ProductModule } from './infra/ioc/product/product.module';
 import { TypeOrmDataSource } from './infra/db/database.provider';
 
 @Module({
@@ -22,14 +24,15 @@ import { TypeOrmDataSource } from './infra/db/database.provider';
         return dataSource;
       },
     }),
-    CategoryModule
-
+    CategoryModule,
+    AttributesModule,
+    ProductModule
   ],
   providers: [
     {
       provide: Decrypter,
       useClass: JwtAdapter,
-    },
+    }
   ],
 })
-export class AppModule { }
+export class AppModule {}
