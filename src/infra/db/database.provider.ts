0d0a48e3bc710273243307fb4c41c06791f45f26
Diego Vieira <dvsdiegovieira@gmail.com>
2024-04-25 07:35:16 -0300

    update: database conection

--- src/infra/db/database.provider.ts
+++ src/infra/db/database.provider.ts
@@ -3,19 +3,17 @@ import { DataSource } from 'typeorm';
 import { join } from 'path';
 import { Env } from '../config/environments';
 
-console.log(join(__dirname, 'typeorm/schemas/*.schema.{js,ts}'));
-
 export const TypeOrmDataSource = new DataSource({
   type: 'postgres',
-  host: Env.DB_HOST,
-  port: Number(Env.DB_PORT),
-  username: Env.DB_USERNAME,
-  password: Env.DB_PASSWORD,
-  database: Env.DB_NAME,
+  host: Env.DATABASE_HOST,
+  port: Number(Env.DATABASE_PORT),
+  username: Env.DATABASE_USERNAME,
+  password: Env.DATABASE_PASSWORD,
+  database: Env.DATABASE_NAME,
   synchronize: false,
-  logger: 'advanced-console',
   entities: [join(__dirname, 'typeorm/schemas/*.schema.{js,ts}')],
   migrationsTableName: 'migrations',
   migrations: [join(__dirname, 'typeorm/migrations/*.{js,ts}')],
   namingStrategy: new SnakeNamingStrategy(),
 });
+
